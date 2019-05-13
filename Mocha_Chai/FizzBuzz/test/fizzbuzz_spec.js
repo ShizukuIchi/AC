9afc5af57c07fac0657225f3c5f593a0c3b225e1
Archer Huang <mmosconii@gmail.com>
2019-05-13 15:38:50 +0800

    Modify Test Message

--- Mocha_Chai/FizzBuzz/test/fizzbuzz_spec.js
+++ Mocha_Chai/FizzBuzz/test/fizzbuzz_spec.js
@@ -10,19 +10,19 @@
 
 let should = chai.should()
 describe('FizzBuzz 測試', function () {
-  it('[Case 1] input: 4 , expect output: 4', function () {
+  it('[Case 1] input: 4 , expect output: 4, should not divided by 3 & 5 and return 4 result', function () {
     const result = fizzBuzz(4)
     result.should.be.equal(4)
   })
-  it('[Case 2] input: 6 , expect output: Fizz', function () {
+  it('[Case 2] input: 6 , expect output: Fizz, should divided by 3 and return Fizz result', function () {
     const result = fizzBuzz(6)
     result.should.be.equal('Fizz')
   })
-  it('[Case 3] input: 10, expect output: Buzz', function () {
+  it('[Case 3] input: 10, expect output: Buzz, should divided by 5 and return Buzz result', function () {
     const result = fizzBuzz(10)
     result.should.be.equal('Buzz')
   })
-  it('[Case 4] input: 30, expect output: FizzBuzz', function () {
+  it('[Case 4] input: 30, expect output: FizzBuzz, should divided by 3 & 5 and return FizzBuzz result', function () {
     const result = fizzBuzz(30)
     result.should.be.equal('FizzBuzz')
   })
