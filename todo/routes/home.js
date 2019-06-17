152d07049dfc07961a0127938a3ac32794982eb5
ArcherHuang <mmosconii@gmail.com>
2019-06-17 23:51:47 +0800

    Add todos and home routers

---
+++ todo/routes/home.js
@@ -0,0 +1,18 @@
+// routes/todo.js
+const express = require('express')
+const router = express.Router()
+const Todo = require('../models/todo')
+
+
+// Todo 首頁
+router.get('/', (req, res) => {
+  Todo.find({})
+    .sort({ name: 'asc' })
+    .exec((err, todos) => {
+      if (err) return console.error(err)
+      return res.render('index', { todos: todos })
+    })
+})
+
+// 設定 /todos 路由
+module.exports = router
