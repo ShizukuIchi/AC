026a3a2b799de31a3f05ace1ba4ac1abd1b20980
ArcherHuang <mmosconii@gmail.com>
2019-06-20 22:18:47 +0800

    【 優化 】將使用者所選擇的排序顯示於選單上

--- RestaurantList_CRUD_SortMenu/handlebars-helpers.js
+++ RestaurantList_CRUD_SortMenu/handlebars-helpers.js
@@ -3,3 +3,7 @@ const Handlebars = require('handlebars')
 Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this)
 })
+
+Handlebars.registerHelper('assign', function (name, value, context) {
+  this[name] = value
+})
