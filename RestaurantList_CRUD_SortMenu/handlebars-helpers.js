ef9e358351836a858e6d6c245cc789358794fcee
ArcherHuang <mmosconii@gmail.com>
2019-06-22 20:56:45 +0800

    fixed #1

--- RestaurantList_CRUD_SortMenu/handlebars-helpers.js
+++ RestaurantList_CRUD_SortMenu/handlebars-helpers.js
@@ -1,7 +1,7 @@
 const Handlebars = require('handlebars')
 
 Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
-  return (arg1 == arg2) ? options.fn(this) : options.inverse(this)
+  return (arg1 === arg2) ? options.fn(this) : options.inverse(this)
 })
 
 Handlebars.registerHelper('assign', function (name, value, context) {
